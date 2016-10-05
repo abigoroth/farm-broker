7c237141928f16e71157dbc6e0a52351269288ff
wanazleen <azleenfauzana@localhost.my>
2016-10-05 19:17:43 +0800

    update district and state

--- app/models/district.rb
+++ app/models/district.rb
@@ -1,12 +1,11 @@
 class District < ApplicationRecord
-<<<<<<< HEAD
+
 belongs_to :state
-=======
+
 
 	has_many :farmsites
   belongs_to :state
   has_many :bidders
   has_many :brokers
 
->>>>>>> de9f3590f7bec16feb9dfd6be2f6c5c8e5621320
 end
--- app/models/state.rb
+++ app/models/state.rb
@@ -1,7 +1,5 @@
 class State < ApplicationRecord
-<<<<<<< HEAD
-has_many :district
-=======
+
   has_many :districts
->>>>>>> de9f3590f7bec16feb9dfd6be2f6c5c8e5621320
+
 end
