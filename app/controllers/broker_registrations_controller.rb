67dce43480d3bd4036a7eb1d4c4e2d6e05adef97
Anis ruby <aniskillerz@gmail.com>
2016-10-06 00:07:28 +0800

    update both registrations controller

--- app/controllers/broker_registrations_controller.rb
+++
@@ -1,6 +0,0 @@
-class Brokers::RegistrationsController < Devise::RegistrationsController
-  private
-    def sign_up_params
-      params.require(:broker).permit(:name, :dob, :address, :phone, :account_no, :email, :password, :password_confirmation)
-    end
-end
