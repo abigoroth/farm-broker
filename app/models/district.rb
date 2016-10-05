9b39bac7773a8c32e23e2f7c66cb94366341a7e9
Fairus <fairuszulkifli93@gmail.com>
2016-10-05 19:54:57 +0800

    update

--- app/models/district.rb
+++ app/models/district.rb
@@ -1,6 +1,6 @@
 class District < ApplicationRecord
 
-	has_many :farmsites
+  has_many :farmsites
   belongs_to :state
   has_many :bidders
   has_many :brokers
--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161005092504) do
+ActiveRecord::Schema.define(version: 20161005093540) do
 
   create_table "bid_processes", force: :cascade do |t|
     t.decimal  "price"
     t.datetime "bidding_time"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
-
     t.integer  "bid_id"
     t.integer  "bidder_id"
     t.index ["bid_id"], name: "index_bid_processes_on_bid_id"
     t.index ["bidder_id"], name: "index_bid_processes_on_bidder_id"
-
   end
 
   create_table "bidders", force: :cascade do |t|
@@ -59,12 +57,10 @@ ActiveRecord::Schema.define(version: 20161005092504) do
     t.decimal  "highest_price"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
-
     t.integer  "broker_id"
     t.integer  "produce_id"
     t.index ["broker_id"], name: "index_bids_on_broker_id"
     t.index ["produce_id"], name: "index_bids_on_produce_id"
-
   end
 
   create_table "brokers", force: :cascade do |t|
@@ -127,4 +123,5 @@ ActiveRecord::Schema.define(version: 20161005092504) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
 end
