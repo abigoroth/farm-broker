23e2b26811ea341a96ab23c9bb0ac38467c392b1
Anis ruby <aniskillerz@gmail.com>
2016-10-05 19:31:58 +0800

    updated v2

--- app/controllers/bidders/registrations_controller.rb
+++ app/controllers/bidders/registrations_controller.rb
@@ -40,9 +40,13 @@ class Bidders::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
    def configure_sign_up_params
-     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :dob, :address, :phone, :credit_card])
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
    end
 
+  private
+  def sign_up_params
+    params.require(:bidder).permit(:name, :dob, :address,:phone,:credit_card,:email, :password, :password_confirmation)
+  end
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
   #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
