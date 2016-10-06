d81d4e8f151b14c10e270d9616eece31516108c4
Kim Sara <sarahazizan@localhost.my>
2016-10-06 14:05:31 +0800

    edit bid

--- db/schema.rb
+++ db/schema.rb
@@ -95,6 +95,13 @@ ActiveRecord::Schema.define(version: 20161006040611) do
     t.datetime "updated_at",           null: false
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.string   "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "produces", force: :cascade do |t|
     t.string   "producename"
     t.string   "producetype"
