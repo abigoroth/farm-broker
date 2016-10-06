acda2c07e2c01923c5103c107ee42188e4029b58
Fairus <fairuszulkifli93@gmail.com>
2016-10-06 14:39:46 +0800

    update

--- db/schema.rb
+++ db/schema.rb
@@ -94,13 +94,6 @@ ActiveRecord::Schema.define(version: 20161006054118) do
     t.datetime "updated_at",           null: false
   end
 
-  create_table "posts", force: :cascade do |t|
-    t.string   "title"
-    t.string   "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "produces", force: :cascade do |t|
     t.string   "producename"
     t.string   "producetype"
