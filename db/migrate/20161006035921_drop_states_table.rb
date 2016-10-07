1a11e1e35b3aad8cbaaa5ed4d960f66d356a70a6
Kim Sara <sarahazizan@localhost.my>
2016-10-07 11:24:58 +0800

    done edit bid bid process

--- db/migrate/20161006035921_drop_states_table.rb
+++
@@ -1,8 +0,0 @@
-class DropStatesTable < ActiveRecord::Migration[5.0]
-  def change
-    drop_table :states
-  end
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
--- db/migrate/20161006040054_drop_districts_table.rb
+++
@@ -1,9 +0,0 @@
-class DropDistrictsTable < ActiveRecord::Migration[5.0]
-  def change
-    drop_table :districts
-    end
-
-    def down
-      raise ActiveRecord::IrreversibleMigration
-    end
-end
