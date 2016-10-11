1232f9073e405f9272609d08221178a067081fcf
Anis ruby <anissoraya@localhost.my>
2016-10-11 11:00:59 +0800

    updated

--- app/models/bidder.rb
+++ app/models/bidder.rb
@@ -12,4 +12,9 @@ class Bidder < ApplicationRecord
 
   has_attached_file :profilephoto, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :profilephoto, content_type: /\Aimage\/.*\z/
+
+  after_create :welcome_send
+  def welcome_send
+    WelcomeMailer.welcome_send(self).deliver
+  end
 end
