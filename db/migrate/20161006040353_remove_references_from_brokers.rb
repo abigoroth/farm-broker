62ea637dfc495a3ec4980cc6422e87f596ee1f09
Fairus <fairuszulkifli93@gmail.com>
2016-10-12 18:43:37 +0800

    buang foreign key

--- db/migrate/20161006040353_remove_references_from_brokers.rb
+++
@@ -1,5 +0,0 @@
-class RemoveReferencesFromBrokers < ActiveRecord::Migration[5.0]
-  def change
-    remove_reference :brokers, :district, index: true, foreign_key: true
-  end
-end
--- db/migrate/20161006040611_remove_ref_from_bidders.rb
+++
@@ -1,5 +0,0 @@
-class RemoveRefFromBidders < ActiveRecord::Migration[5.0]
-  def change
-    remove_reference :bidders, :district, index: true, foreign_key: true
-  end
-end
