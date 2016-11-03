c88e9cecec2e9e637aed7a40718649ffdd99c7aa
Fairus <fairuszulkifli93@gmail.com>
2016-11-03 15:47:44 +0800

    farm

--- app/models/farmsite.rb
+++ app/models/farmsite.rb
@@ -2,8 +2,10 @@ class Farmsite < ApplicationRecord
 	belongs_to :farmer
 	has_many :produces, through: :farmsites
 		
-	geocoded_by :latitude
-	after_validation :geocode
+	geocoded_by :farmsiteaddress
+
+	#geocoded_by :latitude
+	after_validation :geocode  , :if => :address_changed?
  
 end
 
