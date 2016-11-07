dd5471729bd0b9f564f82da7e14760d7b60f7499
Fairus <fairuszulkifli93@gmail.com>
2016-11-07 13:02:30 +0800

    location

--- app/controllers/farmsites_controller.rb
+++ app/controllers/farmsites_controller.rb
@@ -97,6 +97,6 @@ class FarmsitesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def farmsite_params
-      params.require(:farmsite).permit(:farmsitename, :farmsitesize, :farmsiteaddress, :farmsiteownername, :farmsiteownerphoneno, :broker_id)
+      params.require(:farmsite).permit(:farmsitename, :farmsitesize, :farmsiteaddress, :farmsiteownername, :farmsiteownerphoneno, :broker_id, :latitude, :longitude)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161102054107) do
+ActiveRecord::Schema.define(version: 20161107021209) do
 
   create_table "bid_processes", force: :cascade do |t|
     t.decimal  "price"
@@ -180,6 +180,8 @@ ActiveRecord::Schema.define(version: 20161102054107) do
     t.datetime "confirmation_sent_at"
     t.datetime "created_at",                                null: false
     t.datetime "updated_at",                                null: false
+    t.float    "longitude"
+    t.float    "latitude"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
