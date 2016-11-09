78d58e3216b5c961891b5ddb63067e512756f394
Kim Sara <sarahazizan@localhost.my>
2016-11-09 10:07:55 +0800

    merge schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20161108034206) do
-
 
   create_table "bid_processes", force: :cascade do |t|
     t.decimal  "price"
@@ -99,8 +97,8 @@ ActiveRecord::Schema.define(version: 20161108034206) do
     t.integer  "farmsiteownerphoneno"
     t.integer  "district_id"
     t.integer  "farmer_id"
-    t.float    "longitude"
-    t.float    "latitude"
+    t.string   "longitude"
+    t.string   "latitude"
     t.datetime "created_at",           null: false
     t.datetime "updated_at",           null: false
   end
